acecefe10a8041c7943b85383557932780546a71
PaoloGuilen <u202124343@upc.edu.pe>
2024-05-18 21:30:58 -0500

    feat: added more things to the Crops BC

---
+++ ChaquitacllaError404.API/Crops/Domain/Model/Aggregates/Sowing.cs
@@ -0,0 +1,26 @@
+﻿using ChaquitacllaError404.API.Crops.Domain.Model.Commands;
+
+namespace ChaquitacllaError404.API.Crops.Domain.Model.Aggregates;
+
+public class Sowing
+{
+    public int Id { get; private set; }
+    public DateOnly StartDate { get; private set; }
+    public DateOnly EndDate { get; private set; }
+    public int AreaLand { get; private set; }
+    public bool Status { get; private set; }
+    protected Sowing()
+    {
+        this.StartDate = DateOnly.MinValue;
+        this.EndDate = DateOnly.MinValue;
+        this.AreaLand = 0;
+        this.Status = false;
+    }
+
+    public Sowing(CreateSowing command)
+    {
+        this.StartDate = command.StartDate;
+        this.EndDate = command.EndDate;
+        this.AreaLand = command.AreaLand;
+    }
+}
---
+++ ChaquitacllaError404.API/Crops/Domain/Model/Commands/CreateSowing.cs
@@ -0,0 +1,3 @@
+﻿namespace ChaquitacllaError404.API.Crops.Domain.Model.Commands;
+
+public record CreateSowing(DateOnly StartDate, DateOnly EndDate,int AreaLand);
