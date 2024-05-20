a7b69051dfeba74abefe37c42a24a2d1ec3b662c
PaoloGuilen <u202124343@upc.edu.pe>
2024-05-19 20:13:01 -0500

    add: more aggregates

---
+++ ChaquitacllaError404.API/Crops/Domain/Model/Aggregates/Crop.cs
@@ -0,0 +1,22 @@
+﻿using ChaquitacllaError404.API.Crops.Domain.Model.Commands;
+
+namespace ChaquitacllaError404.API.Crops.Domain.Model.Aggregates;
+
+public class Crop
+{
+    public int Id { get; private set; }
+    public string Description { get; private set; }
+    public string Name { get; private set; }
+
+
+    protected Crop()
+    {
+        this.Name = string.Empty;
+        this.Description= string.Empty;
+    }
+    public Crop(CreateCropCommand command)
+    {
+        this.Name= command.Name;
+        this.Description = command.Description; 
+    }
+}
