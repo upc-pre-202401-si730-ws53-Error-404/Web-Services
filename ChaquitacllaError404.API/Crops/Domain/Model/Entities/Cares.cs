c1a0dc1df8deabd96e7b086e94696a259e799bd1
diegocantoralp <u20201f568@upc.edu.pe>
2024-05-31 18:24:33 -0500

    feat(crops): updated cares entity

--- ChaquitacllaError404.API/Crops/Domain/Model/Entities/Cares.cs
+++ ChaquitacllaError404.API/Crops/Domain/Model/Entities/Cares.cs
@@ -1,14 +1,27 @@
-﻿namespace ChaquitacllaError404.API.Crops.Domain.Model.Entities;
+﻿using ChaquitacllaError404.API.Crops.Domain.Model.Aggregates;
+
+namespace ChaquitacllaError404.API.Crops.Domain.Model.Entities;
 
 public class Cares
 {
-    public int Id;
+    public int Id { get; }
 
-    public DateTime control_date { get; private set; }
+    public string description { get; private set; }
     
-    public string condition_description { get; private set; }
+    public DateTime date { get; private set; }
+    
+    public Sowing Sowing { get; private set; }
 
-    public string soil_moisture_description { get; private set; }
-
-
+    public Cares()
+    {
+        
+    }
+    
+    public Cares(string description, DateTime date, Sowing sowing)
+    {
+       this.description = description;
+       this.date = date;
+       Sowing = sowing;
+    }
+    
 }
