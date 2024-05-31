e61eb02de1093b280e7ab11071832b68ffafda95
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-31 18:13:20 -0500

    feat(queries from products to GET): added queries to get all products and products by type.

---
+++ ChaquitacllaError404.API/Crops/Domain/Services/IProductQueryService.cs
@@ -0,0 +1,17 @@
+using ChaquitacllaError404.API.Crops.Domain.Model.Entities;
+using ChaquitacllaError404.API.Crops.Domain.Model.Queries;
+
+namespace ChaquitacllaError404.API.Crops.Domain.Services;
+
+public interface IProductQueryService
+{
+    /**
+     * Return all products
+     */
+    Task<IEnumerable<Product>> Handle(GetAllProductsQuery query);
+    
+    /**
+     * Return products by type
+     */
+    Task<IEnumerable<Product>> Handle(GetProductsByTypeQuery query);
+}
