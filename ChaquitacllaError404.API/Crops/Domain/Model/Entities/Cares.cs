3df2a9cdc959afc181eb015c2282b904b7878e55
diegocantoralp <u20201f568@upc.edu.pe>
2024-05-31 16:39:44 -0500

    feat(crops): rename entity cares

---
+++ ChaquitacllaError404.API/Crops/Domain/Model/Entities/Cares.cs
@@ -0,0 +1,14 @@
+﻿namespace ChaquitacllaError404.API.Crops.Domain.Model.Entities;
+
+public class Cares
+{
+    public int Id;
+
+    public DateTime control_date { get; private set; }
+    
+    public string condition_description { get; private set; }
+
+    public string soil_moisture_description { get; private set; }
+
+
+}
