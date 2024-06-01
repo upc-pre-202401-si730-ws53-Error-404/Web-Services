1e91e761c5ce82def1c39c7d75c157c90487b47b
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-31 20:47:11 -0500

    feat(forum): added userId and question aggregate.

---
+++ ChaquitacllaError404.API/Forum/Domain/Model/Aggregates/Question.cs
@@ -0,0 +1,37 @@
+﻿using ChaquitacllaError404.API.Forum.Domain.Model.Commands;
+using ChaquitacllaError404.API.Forum.Domain.Model.ValueObjects;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace ChaquitacllaError404.API.Forum.Domain.Model.Aggregates;
+
+public class Question : IEntityWithCreatedUpdatedDate
+{
+    public int Id { get; }
+    public string Category { get; private set; }
+    public string Ask { get; private set;}
+    public UserId AuthorId { get; }
+    public DateTimeOffset? CreatedDate { get; set; }
+    public DateTimeOffset? UpdatedDate { get; set; }
+
+    public Question()
+    {
+        Category = string.Empty;
+        Ask = string.Empty;
+    }
+    
+    public Question(string category, string ask)
+    {
+        Category = category;
+        Ask = ask;
+    }
+
+    public Question(CreateQuestionCommand command) : this(command.Category, command.Ask){ }
+
+    public Question UpdateInformation(UpdateQuestionCommand command)
+    {
+        Category = command.Category;
+        Ask = command.Ask;
+        return this;
+    }
+    
+}
