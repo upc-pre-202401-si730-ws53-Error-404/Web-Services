432775731b087eb15e902ba4005d217d0b9f2907
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-31 22:34:57 -0500

    feat(forum): added question repository.

---
+++ ChaquitacllaError404.API/Forum/Infrastructure/Persistence/EFC/Repositories/QuestionRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChaquitacllaError404.API.Forum.Domain.Model.Aggregates;
+using ChaquitacllaError404.API.Forum.Domain.Repositories;
+using ChaquitacllaError404.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ChaquitacllaError404.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace ChaquitacllaError404.API.Forum.Infrastructure.Persistence.EFC.Repositories;
+
+public class QuestionRepository(AppDbContext context) : BaseRepository<Question>(context), IQuestionRepository
+{
+    
+
+    public Task<IEnumerable<Question>> ListByUserIdAsync(int userId)
+    {
+        throw new NotImplementedException();
+        // TODO: Implement this method
+    }
+    
+}
