e0071fe4197418263cd27ebf6ae2110676fe91be
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-06-02 17:04:35 -0500

    feat(forum): added answer controller, resources and transform.

---
+++ ChaquitacllaError404.API/Forum/Interfaces/REST/Transform/CreateAnswerCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using ChaquitacllaError404.API.Forum.Domain.Model.Commands;
+using ChaquitacllaError404.API.Forum.Interfaces.REST.Resources;
+
+namespace ChaquitacllaError404.API.Forum.Interfaces.REST.Transform;
+
+public static class CreateAnswerCommandFromResourceAssembler
+{
+    public static CreateAnswerCommand ToCommandFromResource(int authorId,int questionId,CreateAnswerResource resource)
+    {
+        return new CreateAnswerCommand(authorId, questionId, resource.AnswerText);
+    }
+}
