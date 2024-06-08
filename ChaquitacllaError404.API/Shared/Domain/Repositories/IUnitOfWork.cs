f6b0fa60cf87fa1150600a3450e5fc1a0d68d0a9
jufer10 <fcarrasco023@gmail.com>
2024-06-08 00:56:15 -0500

    refactor:  deleted all unused things in this branch

--- ChaquitacllaError404.API/Shared/Domain/Repositories/IUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-namespace ChaquitacllaError404.API.Shared.Domain.Repositories;
-
-public interface IUnitOfWork
-{
-    Task CompleteAsync();
-}
--- ChaquitacllaError404.API/Users/Domain/Model/Entities/City.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ChaquitacllaError404.API.Users.Domain.Model.Entities;
-
-public class City
-{
-    
-}
