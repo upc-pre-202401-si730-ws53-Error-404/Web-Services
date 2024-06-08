11d1bb54b46fa405edf8a13f216d3e846cca0c70
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:59:10 -0500

    feat(users): added user query service in application layer

---
+++ ChaquitacllaError404.API/Users/Application/Internal/QueryServices/UserQueryService.cs
@@ -0,0 +1,28 @@
+﻿using ChaquitacllaError404.API.Users.Domain.Model.Aggregates;
+using ChaquitacllaError404.API.Users.Domain.Model.Queries;
+using ChaquitacllaError404.API.Users.Domain.Repositories;
+
+namespace ChaquitacllaError404.API.Users.Application.Internal.QueryServices;
+
+public class UserQueryService(IUserRepository userRepository)
+{
+    public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query)
+    {
+        return await userRepository.ListAsync();
+    }
+
+    public async Task<User?> Handle(GetUserByIdQuery query)
+    {
+        return await userRepository.FindByIdAsync(query.UserId);
+    }
+    
+    public async Task<User?> Handle(GetUserByFirstNameQuery query)
+    {
+        return await userRepository.FindByFirstNameAsync(query.FirstName);
+    }
+    
+    public async Task<User?> Handle(GetUserByLastNameQuery query)
+    {
+        return await userRepository.FindByLastNameAsync(query.LastName);
+    }
+}
