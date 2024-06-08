21c8de260eed180e9f9482ccf701113f3abc6b64
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:52:34 -0500

    feat(shared): added user entity in appdbcontext

---
+++ ChaquitacllaError404.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using ChaquitacllaError404.API.Users.Domain.Model.Aggregates;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        // Enable Audit Fields Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+{
+    base.OnModelCreating(builder);
+
+    // Users Context
+    builder.Entity<User>().ToTable("Users");
+    builder.Entity<User>().HasKey(u => u.Id);
+    builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<User>().Property(u => u.FirstName).IsRequired();
+    builder.Entity<User>().Property(u => u.LastName).IsRequired();
+    builder.Entity<User>().Property(u => u.Password).IsRequired();
+    builder.Entity<User>().Property(u => u.Email).IsRequired();
+    builder.Entity<User>().Property(u => u.Subscription).IsRequired();
+
+    // Apply SnakeCase Naming Convention
+    builder.UseSnakeCaseWithPluralizedTableNamingConvention();
+}
+
+
+}
