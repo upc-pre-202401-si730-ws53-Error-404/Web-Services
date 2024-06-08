04fccddec293dcb76ba03a524c640b5267ecda98
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:56:35 -0500

    feat(shared): added kebab case convention for database table creation

---
+++ ChaquitacllaError404.API/Shared/Infrastructure/Interfaces/ASP/Configuration/KebabCaseRouteNamingConvention.cs
@@ -0,0 +1,23 @@
+﻿using FlexPalPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+
+namespace FlexPalPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+public class KebabCaseRouteNamingConvention : IControllerModelConvention
+{
+    private static AttributeRouteModel? ReplaceControllerTemplate(SelectorModel selector, string name)
+    {
+        return selector.AttributeRouteModel != null 
+            ? new AttributeRouteModel { Template = selector.AttributeRouteModel.Template?.Replace("[controller]", name.ToKebabCase()) } 
+            : null;
+    }
+    public void Apply(ControllerModel controller)
+    {
+        foreach (var selector in controller.Selectors) 
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+
+        foreach (var selector in controller.Actions.SelectMany(a => a.Selectors)) 
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+        
+    }
+}
