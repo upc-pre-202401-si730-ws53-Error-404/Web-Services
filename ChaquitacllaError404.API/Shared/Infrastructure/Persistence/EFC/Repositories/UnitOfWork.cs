7c43f7286130d7abb0e6e9915a49636a0f5a91e1
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:53:54 -0500

    feat(shared):  added configuration and persistences in infrastructure layer

---
+++ ChaquitacllaError404.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using FlexPalPlatform.API.Shared.Domain.Repositories;
+using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
