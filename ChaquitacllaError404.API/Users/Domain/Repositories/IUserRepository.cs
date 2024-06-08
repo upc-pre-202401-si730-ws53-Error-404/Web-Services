bf24b5bc9da4e8b09618529aafbbe2b6941749f3
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:57:50 -0500

    feat(users): added interfaces to override them in the future

---
+++ ChaquitacllaError404.API/Users/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChaquitacllaError404.API.Users.Domain.Model.Aggregates;
+using FlexPalPlatform.API.Shared.Domain.Repositories;
+
+namespace ChaquitacllaError404.API.Users.Domain.Repositories;
+
+public interface IUserRepository: IBaseRepository<User>
+{
+    Task<User?> FindByFirstNameAsync(string firstName);
+    Task<User?> FindByLastNameAsync(string lastName);
+}
---
+++ ChaquitacllaError404.API/Users/Domain/Services/IUserCommandService.cs
@@ -0,0 +1,12 @@
+﻿using ChaquitacllaError404.API.Users.Domain.Model.Aggregates;
+using ChaquitacllaError404.API.Users.Domain.Model.Commands;
+using ChaquitacllaError404.API.Users.Domain.Model.ValueObjects;
+
+namespace ChaquitacllaError404.API.Users.Domain.Services;
+
+public interface IUserCommandService
+{
+    Task<User?> Handle(CreateUserCommand command);
+    Task<User?> Handle(CreateUserCommand command, string firstName, string lastName, string email, Subscriptions subscription);
+    Task<string?> Handle(LoginUserCommand command);
+}
