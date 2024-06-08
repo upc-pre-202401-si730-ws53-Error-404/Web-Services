9730422b8762440c8c760c9f69b73091a022fe93
jufer10 <fcarrasco023@gmail.com>
2024-06-08 01:58:39 -0500

    feat(users): added user repository on infrastructure layer

---
+++ ChaquitacllaError404.API/Users/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChaquitacllaError404.API.Users.Domain.Model.Aggregates;
+using ChaquitacllaError404.API.Users.Domain.Repositories;
+using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FlexPalPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChaquitacllaError404.API.Users.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context): BaseRepository<User>(context), IUserRepository
+{
+    public async Task<User?> FindByFirstNameAsync(string firstName)
+    {
+        return await context.Set<User>().FirstOrDefaultAsync(user => user.FirstName == firstName);
+    }
+
+    public async Task<User?> FindByLastNameAsync(string lastName)
+    {
+        return await context.Set<User>().FirstOrDefaultAsync(user => user.FirstName == lastName);
+    }
+}
