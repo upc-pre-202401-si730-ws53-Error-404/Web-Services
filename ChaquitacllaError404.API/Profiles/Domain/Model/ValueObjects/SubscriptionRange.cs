9a999f64e42c67f7835d96e84acd442c3ff693df
jufer10 <fcarrasco023@gmail.com>
2024-06-16 01:31:10 -0500

    feat(profiles): added domain layer

---
+++ ChaquitacllaError404.API/Profiles/Domain/Model/ValueObjects/SubscriptionRange.cs
@@ -0,0 +1,6 @@
+﻿namespace ChaquitacllaError404.API.Profiles.Domain.Model.ValueObjects;
+
+public enum SubscriptionRange
+{
+    
+}
---
+++ ChaquitacllaError404.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChaquitacllaError404.API.Profiles.Domain.Model.Aggregates;
+using ChaquitacllaError404.API.Profiles.Domain.Model.ValueObjects;
+using ChaquitacllaError404.API.Shared.Domain.Repositories;
+
+namespace ChaquitacllaError404.API.Profiles.Domain.Repositories;
+
+public interface IProfileRepository : IBaseRepository<Profile>
+{
+    Task<Profile?> FindProfileByEmailAsync(EmailAddress email);
+}
