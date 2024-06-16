1fdd748f4ea8a9e1a19c0a5edfe90a0c004d05e5
jufer10 <fcarrasco023@gmail.com>
2024-06-16 02:33:22 -0500

    feat(profiles): added repository, and services for subscription

---
+++ ChaquitacllaError404.API/Profiles/Domain/Model/ValueObjects/SubscriptionTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace ChaquitacllaError404.API.Profiles.Domain.Model.ValueObjects;
+
+public enum SubscriptionTypes
+{
+    Test_Subscription = 1,
+    Anual_Subscription = 2
+}
+public static class SubscriptionRange
+{
+    public static string ToDescription(this SubscriptionTypes range)
+    {
+        switch (range)
+        {
+            case SubscriptionTypes.Test_Subscription:
+                return "Test_Subscription";
+            case SubscriptionTypes.Anual_Subscription:
+                return "Anual_Subscription";
+            // Agrega más aquí si es necesario
+            default:
+                return "Unknown Subscription Range";
+        }
+    }
+}
